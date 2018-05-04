3bfd49ff17fe95a7b52493e5b3dae9bdaf96f2a2
KarinFF <karinfrostell@gmail.com>
2018-05-04 10:43:06 +0200

    starting on admin site

--- frontend/src/components/admin-view/index.js
+++ frontend/src/components/admin-view/index.js
@@ -1,11 +1,53 @@
 import React from "react"
 
 export default class AdminView extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      answer: ""
+    }
+    }
+  handleAnswerSubmit = event => {
+    event.preventDefault()
+    fetch("http://localhost:8081/faq", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, textplain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }).then(response => {
+      if (response.status === 201) {
+        this.setState({
+          answer: ""
+        })
+      }
+    }).catch(err => {
+      // api down? request failed?
+      console.log("Error!", err)
+    })
+  }
+
+  handleAnswer = event => {
+    this.setState({
+      answer: event.target.value
+    })
+  }
 
   render() {
     return (
       <div>
-        Admin here
+        <form onSubmit={this.handleAnswerSubmit}>
+          <label>
+            <p>Enter your answer</p>
+            <input
+              name="answer"
+              type="text"
+              value={this.state.answer}
+              onChange={this.handleAnswer} />
+          </label>
+          <button>Send</button>
+        </form>
       </div>
     )
   }
