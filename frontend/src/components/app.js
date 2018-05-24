6fd8c9d6663f1ce60648d067e1aab60753ad66ef
elise offerman <eliseofferman@hotmail.com>
2018-05-24 16:51:46 +0200

    added HashRouter

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { BrowserRouter, Route } from "react-router-dom"
+import { HashRouter, Route } from "react-router-dom"
 import Navigation from "./navigation"
 import HomeView from "./home-view"
 import AdminView from "./admin-view"
@@ -8,14 +8,14 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
 
         <div>
           <Navigation />
           <Route exact path="/" component={HomeView} />
           <Route path="/admin" component={AdminView} />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 
